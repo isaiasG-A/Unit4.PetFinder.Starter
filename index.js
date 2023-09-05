071261d8efcbaaaddaa82f95bd44b50eac657e79
isaiasG-A <isaiasgarciaariza@gmail.com>
2023-09-05 15:56:34 -0700

    all express routes have been completed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = 8080;
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+    res.sendFile(__dirname  + '/public/index.html')
 });
 
 // hello world route
@@ -21,31 +21,35 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.send(pets);
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    const owner = req.query.owner;
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
-
     // send the pet as a response
+    res.send(pet);
 
+    //http://localhost:8080/api/v1/pets/owner?owner=Jane
 });
 
-// get pet by name
+// get pet by :name(params)
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
+    const name = req.params.name;
+    console.log(name)
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
+    res.send(pet);
 
+    //http://localhost:8080/api/v1/pets/Rover
 });
 
 app.listen(PORT, () => {
